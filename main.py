27bfa1c6aaa7cb9a13e3be9c184b2c2c8340cdcc
Jake Strouse <jakestrouse00@gmail.com>
2024-02-20 22:33:48 -0500

    can now add field from json_to_code

--- main.py
+++ main.py
@@ -26,33 +26,33 @@ def create_object(name: str, arguments: List[ArgumentCodeBlock]) -> ClassCodeBlo
     return ClassCodeBlock(f"{name}", arguments)
 
 
-def d2_process_json(data: dict) -> Tuple[List[ArgumentCodeBlock], List[ClassCodeBlock]]:
+def d2_process_json(data: dict, field: FieldInput | None = None) -> Tuple[List[ArgumentCodeBlock], List[ClassCodeBlock]]:
     # d2 = a dict with 1 dict inside of it
     fields: List[ArgumentCodeBlock] = []
     child_classes = []
     for index, (key, value) in enumerate(data.items()):
         if isinstance(value, dict):
-            child_object_data = d2_process_json(value)
+            child_object_data = d2_process_json(value, field=field)
             child_object = create_object(key, child_object_data[0])
             for obj in child_object_data[1]:
                 child_classes.append(obj)
             child_classes.append(child_object)
 
-            arg = ArgumentCodeBlock(key, child_object.name, field=None)
+            arg = ArgumentCodeBlock(key, child_object.name, field=field)
         else:
-            arg = ArgumentCodeBlock(key, type_hint=type(value), field=None)
+            arg = ArgumentCodeBlock(key, type_hint=type(value), field=field)
         fields.append(arg)
 
     return fields, child_classes
 
 
-def json_to_code(data: dict) -> FileCodeBlock:
-    args = d2_process_json(data)
+def json_to_code(data: dict, field: FieldInput | None = None) -> FileCodeBlock:
+    args = d2_process_json(data, field=field)
     test_obj = ClassCodeBlock("Person", args[0])
     code = FileCodeBlock(main_object=test_obj, child_objects=args[1], imports=[FromImport("pydantic.dataclasses", ["dataclass", "Field"])])
     return code
 
 
 file = load_file("test2.json")
-code = json_to_code(file)
+code = json_to_code(file, FieldInput(repr=False))
 write_file("testing", code)
